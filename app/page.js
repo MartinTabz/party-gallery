e759601c27744719ef4067156052961bbf7f8a80
Martin Šíl <mail@martinsil.cz>
2024-03-08 21:18:04 +0100

    Main page with QR code generation

--- app/page.js
+++ app/page.js
@@ -1,3 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import QRCode from "react-qr-code";
+
 export default function Home() {
-	return <></>;
+	const uploadUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/nahrat`;
+
+	return (
+		<main>
+			<h2>Party Gallery</h2>
+			<QRCode
+				size={320}
+        style={{ margin: "50px"}}
+				value={uploadUrl}
+				viewBox={`0 0 320 320`}
+			/>
+      <br />
+			<Link href={uploadUrl}>Připojit se</Link>
+		</main>
+	);
 }
