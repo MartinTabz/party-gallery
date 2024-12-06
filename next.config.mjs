9398912c14e8e8d75b30de0d9db4550e3eedc4a7
Martin Šíl <mail@martinsil.cz>
2024-12-06 08:41:59 +0100

    Updating Next and Supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-     domains: ['hywzenpeiszdkkozmvvs.supabase.co'],
+     domains: ['kfjplqsbplhpabqcdmgm.supabase.co'],
    },
  };
 
